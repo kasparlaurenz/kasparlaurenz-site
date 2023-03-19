2b9818bfb5eb41a72b49c1b93659128eae2ba408
Laurenz Seipel <lseipel@pagemachine.de>
2023-03-19 23:58:42 +0100

    Work on index page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,19 @@
+import Head from "next/head";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg h-screen w-full bg-neutral-900 text-slate-100">
-      {children}
-    </div>
+    <>
+      <Head>
+        <title>kasparlaurenz</title>
+        <meta name="description" content="kasparlaurenz" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg h-screen w-full bg-neutral-900 text-slate-100">
+        <div className="mx-auto h-screen w-full max-w-5xl bg-neutral-900">
+          {children}
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,37 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import BigNav from "~/components/Navigation/BigNav";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
 
-const Home: NextPage = () => {
+const Index: NextPage = () => {
+  const drag = useMotionValue(0);
+  const opacityLeft = useTransform(drag, [-1000, 0, 200], [0.7, 0, 0]);
+  const opacityRight = useTransform(drag, [-200, 0, 1000], [0, 0, 0.7]);
+
+  console.log(drag.get());
   return (
-    <>
-      <Head>
-        <title>kasparlaurenz</title>
-        <meta name="description" content="kasparlaurenz" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <BigNav isHome />
-    </>
+    <div className="flex h-screen w-full items-center justify-between">
+      <motion.div className="text-3xl" style={{ opacity: opacityLeft }}>
+        Guest
+      </motion.div>
+      <div className="flex flex-col items-center justify-center gap-3">
+        <div className="text-3xl">Drag</div>
+        <motion.div
+          className="z-20 h-6 w-6 cursor-pointer rounded-full bg-yellow-500"
+          whileHover={{ scale: 1.3 }}
+          transition={{ type: "spring", stiffness: 400, damping: 10 }}
+          drag
+          dragSnapToOrigin
+          style={{ x: drag }}
+          dragElastic={0.2}
+          dragConstraints={{ right: 500, left: -500, top: -50, bottom: 50 }}
+          whileDrag={{ scale: 0.8 }}
+        ></motion.div>
+      </div>
+      <motion.div className="text-3xl" style={{ opacity: opacityRight }}>
+        Menu
+      </motion.div>
+    </div>
   );
 };
 
-export default Home;
+export default Index;
